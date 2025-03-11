c1aaa49ed921e027362de4d8899443288259f793
Hatim Meftahi <70101504+hatimm@users.noreply.github.com>
2025-03-11 22:27:04 +0000

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         whileInView={{opacity:1,y:0}}
         initial={{opacity:0,y:-100}}
         transition={{duration:0.5}}
-        className="my-10 text-center text-4xl">Me contacter</motion.h2>
+        className="my-10 text-center text-4xl">N'hésitez pas à me contacter</motion.h2>
         <div className="text-center tracking-tighter">
             <motion.p
             whileInView={{opacity:1,y:0}}
